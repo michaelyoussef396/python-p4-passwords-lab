3737fe258585d2328b099314d9ab306b8bb62d90
michael youssef <michaelyoussef396@gmail.com>
2024-08-16 10:52:21 +1000

    passsed all tests

--- server/app.py
+++ server/app.py
@@ -1,43 +1,65 @@
-#!/usr/bin/env python3
+from flask import Flask, request, session, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from flask_bcrypt import Bcrypt
+from models import db, User
 
-from flask import request, session
-from flask_restful import Resource
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-from config import app, db, api
-from models import User
+app.secret_key = b'a\xdb\xd2\x13\x93\xc1\xe9\x97\xef2\xe3\x004U\xd1Z'
 
-class ClearSession(Resource):
-
-    def delete(self):
-    
-        session['page_views'] = None
-        session['user_id'] = None
-
-        return {}, 204
+db.init_app(app)
+migrate = Migrate(app, db)
+api = Api(app)
+bcrypt = Bcrypt(app)
 
 class Signup(Resource):
-    
     def post(self):
-        json = request.get_json()
-        user = User(
-            username=json['username']
-        )
-        user.password_hash = json['password']
+        data = request.get_json()
+        user = User(username=data['username'])
+        user.password_hash = data['password']
         db.session.add(user)
         db.session.commit()
+        session['user_id'] = user.id
         return user.to_dict(), 201
 
-class CheckSession(Resource):
-    pass
-
 class Login(Resource):
-    pass
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter_by(username=data['username']).first()
+        if user and user.authenticate(data['password']):
+            session['user_id'] = user.id
+            return user.to_dict(), 200
+        return {'error': 'Invalid username or password'}, 401
 
 class Logout(Resource):
-    pass
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
 
-api.add_resource(ClearSession, '/clear', endpoint='clear')
-api.add_resource(Signup, '/signup', endpoint='signup')
+class CheckSession(Resource):
+    def get(self):
+        user_id = session.get('user_id')
+        if user_id:
+            user = User.query.get(user_id)
+            return user.to_dict(), 200
+        return {}, 204
+
+api.add_resource(Signup, '/signup')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
+
+def create_tables():
+    with app.app_context():
+        db.create_all()
+
+# Initialize the database
+create_tables()
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
